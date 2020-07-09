2deb8faf8a8832c7afebf024bf09824dce55fbf8
Jason Wright <jwright@speareducation.com>
2020-07-08 17:26:40 -0700

    GL-1451 added fetch --unshallow

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,11 @@ ACCESS_TOKEN=$(
 
 [ -z "$ACCESS_TOKEN" ] && echo "Failed to authenticate with bitbucket. Please check the client key and secret." && exit 1
 
+# Make sure we fetch all commits
+git fetch --unshallow origin
 
+# Change the origin URL to bitbucket
 git remote set-url origin "https://x-token-auth:$ACCESS_TOKEN@$INPUT_REPOSITORY"
 
+# Push the commits
 git push origin "$(git rev-parse --abbrev-ref HEAD)"
